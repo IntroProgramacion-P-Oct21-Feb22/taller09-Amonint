d3c5fac24da66906423e24435f87b2cdf1388938
Amon <you@example.com>
2021-12-17 11:57:35 -0500

    ...

--- EjerciciosT9/src/paquete1/Ejercicio03.java
+++ EjerciciosT9/src/paquete1/Ejercicio03.java
@@ -16,10 +16,23 @@ public class Ejercicio03 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int[] arreglo = {1, 10, 11, 12, 12, 13, 16, 2, 3, 4, 9, 10, 21};
+        int[] arreglo = { 1, 10, 11, 12, 12, 13, 16, 2, 3, 4, 9, 10, 21 };
         int elementosArribaMedia;
         int elementosAbajoMedia;
-        
-    }
+        double suma = 0;
+        double media =0;
 
+        for (int i = 0; i < arreglo.length; i++) {
+            suma = suma + arreglo[i];
+        }
+        media= suma / arreglo.length;
+        for(int i = 0; i < arreglo.length; i++){
+            if(arreglo[1]< media){
+                elementosAbajoMedia = elementosAbajoMedia + 1;
+            }else{
+                elementosArribaMedia = elementosArribaMedia + 1;
+            }
+        }
+        }
+    }
 }
